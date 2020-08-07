39328650990d35a3a966d88e80a8fb68e2731977
Natasha <technochic@users.noreply.github.com>
2020-08-07 16:47:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
-let strip: neopixel.Strip = null
 input.onButtonPressed(Button.A, function () {
-    music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
-    strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-    strip.showBarGraph(0, 8)
+    for (let index = 0; index < 8; index++) {
+        strip.setBrightness(100)
+        strip.show()
+        music.setVolume(randint(150, 255))
+        music.playTone(randint(250, 500), music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+        strip.showColor(neopixel.rgb(randint(0, 100), randint(0, 100), randint(0, 100)))
+        strip.show()
+    }
 })
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
